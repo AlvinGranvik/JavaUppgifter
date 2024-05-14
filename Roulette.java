e89e8c0d684d217398177a08878f550a22aa4bc8
AlvinGranvik <alvin.granvik@elev.ga.ntig.se>
2024-05-14 10:53:58 +0200

    EPIC UPDATE TRES

--- Roulette.java
+++ Roulette.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Roulette {
     public static int russianroulette() {
         Random random = new Random();
-        return random.nextInt(6) + 1;
+        return random.nextInt(6);
     }
 
     public static void main(String[] args) {
@@ -17,7 +17,7 @@ public class Roulette {
         System.out.println("Press ENTER to roll, there is a 1/6 chance for a fire."); //Russian roulette börjar
         scanner.nextLine(); //Väntar på spelare att trycka på enter
 
-        if (result == 1) { //If sats där om resultatet är 1, så stängs datorn ner. Annars händer ingenting.
+        if (result == 0) { //If sats där om resultatet är 0, så stängs datorn ner. Annars händer ingenting.
             System.out.println("womp womp");
             try { //Try & catch sats där programmet försöker stänga av ens dator
                 Process process = Runtime.getRuntime().exec("shutdown -s -t 0"); //Stänger av datorn
@@ -28,7 +28,7 @@ public class Roulette {
                 System.err.println("Computer didn't shut down idk why");
             }
         } else { //Else sats som egentligen inte gör så mycket alls
-            System.out.println("wow let's go very nice"); //Printar helt enkelt detta om man får ett värde som inte är 1
+            System.out.println("wow let's go very nice"); //Printar helt enkelt detta om man får ett värde som inte är 0
         }
         scanner.close(); //Scanner stängs för att undvika en resource leak
     }
